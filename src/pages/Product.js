c743e8577a54f51596b47ebc6e4999af1168e253
Alfa <900123@telkom.co.id>
2022-06-03 12:12:07 +0700

    fixing warning

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {useNavigate} from 'react-router-dom'
+// import {useNavigate} from 'react-router-dom'
 
 const Product = () => {
   const [category, setCategory] = useState([]);
   console.log("helo");
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
   async function fetchData(){
     const varToken = localStorage.getItem('token')
